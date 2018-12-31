812e8e16f23a8b431063fd07c55cb046b45b2a11
erickLoreto <45906495+erickLoreto@users.noreply.github.com>
2018-12-31 01:34:43 -0300

    video 21

--- Sales/Sales/ViewModels/MainViewModel.cs
+++ Sales/Sales/ViewModels/MainViewModel.cs
@@ -1,16 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Sales.ViewModels
+﻿namespace Sales.ViewModels
 {
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+    using Sales.Views;    
+    using Xamarin.Forms;
     public class MainViewModel
     {
         public ProductsViewModel Products { get; set; }
 
+        public AddProductViewModel AddProduct { get; set; }
+
         public MainViewModel()
         {
             this.Products = new ProductsViewModel();
+        }
+
+        public ICommand AddProductCommand
+        {
+            get
+            {
+                return new RelayCommand(GoToAddProduct);
+            }
+        }
+
+        private async void GoToAddProduct()
+        {
+            this.AddProduct = new AddProductViewModel();
+            await Application.Current.MainPage.Navigation.PushAsync(new AddProductPage());
         }
     }
 }
